19f02c36372d470c716192efa77df72fb6ce2983
KiryuKami <141748918+KiryuKami@users.noreply.github.com>
2024-04-02 07:56:55 -0500

    AA
    saS

--- index.php
+++ index.php
@@ -11,9 +11,12 @@
 <button>SUBMIT</button>
    </form>
     <h1>Karter</h1>
+    
     <a href="calculator.php">calculator page</a>
     <a href="calender.php">Calender</a>
-    <a href="condtionionals.php">condtionionals</a>
+   <a href="condtionionals.php">condtionionals</a>
+    <a href="loops.php">Loops</a>
+   
     <?php
    // Video 8 Code (Aritmeic Operations)
    echo 5+5;
---
+++ loops.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Loops</title></head>
+<body>
+    <?php
+//Video 17 
+$x = 1;
+while (x < 5){
+   echo "Hi there<br>";
+   $x++;
+}
+// End Of Video 17
+
+//Video 18
+$x = 10;
+do{
+    echo "Hi There<br>";
+    $x++
+}
+while ($x <= 5)
+//End of Video 18
+
+//Video 19
+
+for ($x = 1; $x <= 10; $x++){
+    echo "hi<br>";
+}
+
+//End of Video 19 Code//
+
+//Video 20 Code (Foreach Loop)//
+
+$array = array("Kiryu", "Kami", "Varan");
+
+foreach ($array as $loopdata){
+    echo "My name is" .$loopdata."<br>";
+}
+//End of Video 20 Code//
+
+    ?>
+</body>
+</html>
