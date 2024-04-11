777b31edc7d3b01542812127c7ba98a705248e2f
KiryuKami <141748918+KiryuKami@users.noreply.github.com>
2024-04-11 07:48:17 -0500

    assss
    aasss

--- includes/header.php
+++ includes/header.php
@@ -37,6 +37,6 @@ echo $_SESSION ['username'];
 if (!isset($_SESSION['username'])) {
   echo "You are not logged in!";
 } else {
-  echo "You are logged in!";
+  echo "You are logged in!";    
 }
 ?>
